ec8fca03c9998210a3855463113cb65b1a3f7518
keyian <keyian@nyu.edu>
2021-02-22 16:45:27 -0500

    remove key and be done with this bullshit

---
+++ client/src/components/ItemMap.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+console.log(process.env);
+
+class ItemMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 40.7128,
+      lng: -74.0060
+    },
+    zoom: 11
+  };
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GMAPS_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={40.7128}
+            lng={-74.0060}
+            text="Keyian is Gay"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default ItemMap;
