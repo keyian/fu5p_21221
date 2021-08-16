cb228bdffbf8edb0d9b0fd75cac551dd810e8d78
keyian <keyian@nyu.edu>
2021-08-15 23:56:55 -0400

    upload small changes for tomorrow windows work

--- client/src/components/MapMarker.js
+++ client/src/components/MapMarker.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './../App.css';
+//css
+import './styles/MapMarker.css';
 //import anya from 'anya.png';
 //remove the file root...
-const FILE_ROOT = 'client/public';
+const FILE_ROOT = '/client/public';
 const MapMarker = (props) => {
   let source = props.item.img.substring(FILE_ROOT.length);
 
   return (
-    <div>
-        <img src={source} width="25" alt={props.item.name} />
+    <div className="marker">
+        <img className="marker" src={source} width="25" height="25" alt={props.item.name} />
     </div>
   );
 }
