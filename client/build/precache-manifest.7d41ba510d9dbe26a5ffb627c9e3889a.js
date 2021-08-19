e2d49cbb752e9c82749bc9537b61b1f412f21ff7
keyian <keyian@nyu.edu>
2021-08-19 12:19:52 -0400

    lets see if committing build with the necessary images/uploads folder is the solution....

---
+++ client/build/precache-manifest.7d41ba510d9dbe26a5ffb627c9e3889a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b0107e4dcedbc536bce1b162cea446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd7bef487b969f7c957b",
+    "url": "/static/css/main.b97d3e87.chunk.css"
+  },
+  {
+    "revision": "eb7a13208e000ef809de",
+    "url": "/static/js/2.2001d4de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2001d4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7bef487b969f7c957b",
+    "url": "/static/js/main.1631a395.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
