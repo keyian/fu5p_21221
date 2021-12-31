3c245edc44c8e64ffbb762d1bba5eac9d551084e
keyian <keyian@nyu.edu>
2021-12-31 16:52:59 -0500

    home now conforming to bootstrap layout

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,11 @@ import Home from './components/Home';
 import User from './components/User';
 import Item from './components/Item';
 
+//react-bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 //css
 import './App.css';
 
@@ -21,10 +26,15 @@ function App(props) {
     return(
       <div className="app">
         <AppContextProvider>
-          <Header />
-          <Route exact path="/"><Home /></Route>
-          <Route path="/user/:user"><User /></Route>
-          <Route path="/item/:item"><Item /></Route>
+          <Container>
+            <Row><Header /></Row>
+            <Row>
+              <Route exact path="/"><Home /></Route>
+              <Route path="/user/:user"><User /></Route>
+              <Route path="/item/:item"><Item /></Route>
+            </Row>
+          </Container>
+          
         </AppContextProvider>
       </div>
     );
