9fcc9c09efbb93d5ffff8bbe1bb8315252e87c46
keyian <keyian@nyu.edu>
2022-01-13 21:09:23 -0500

    test worked, environment wasn't pulling correctly

--- knex/knex.js
+++ knex/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.ENVIRONMENT || 'development'
+const environment = process.env.NODE_ENV || 'development'
 console.log("THIS IS RESULT OF ENVIRONMENT...", environment);
 const config = require('../knexfile.js')[environment];
 module.exports = require('knex')(config);
