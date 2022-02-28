f5fde0eb009423c1b1051a8f6f803ea263a689b9
keyian <keyian@nyu.edu>
2022-02-27 21:03:43 -0500

    forgot validator

---
+++ client/src/helpers/itemFormValidator.js
@@ -0,0 +1,8 @@
+//take in every field submitted, scrutinize,
+// correct and return proper errors if needed.
+
+const itemFormValidator = () => {
+    console.log("this is itemformvalidator");
+}
+
+export default itemFormValidator;
